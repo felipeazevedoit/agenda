9117a22cefc3b64d7bf802b5294d40713b921a6a
Felipe Michel de Azevedo <felipe.azevedoit@outlook.com>
2020-02-10 19:13:58 -0300

    Configurando repositorio base

--- Agenda.Dominio/Base/IBase.cs
+++ Agenda.Dominio/Base/IBase.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Agenda.Dominio.Base
 {
@@ -9,6 +7,5 @@ namespace Agenda.Dominio.Base
         public T Cadastrar(T obj);
         public T Alterar(T obj);
         public IEnumerable<T> Listar();
-        public IEnumerable<T> ListarAtivos();
     }
 }
---
+++ Agenda.Repositorio/Base/BaseRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using Agenda.Infraestrutura;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Agenda.Repositorio.Base
+{
+    public class BaseRepositorio<T> : Dominio.Base.IBase<T> where T : class
+    {
+        private readonly Contexto Db = new Contexto();
+        public T Alterar(T obj)
+        {
+           
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+            Db.Dispose();
+            return obj;
+        }
+        public T Cadastrar(T obj)
+        {
+            Db.Entry(obj).State = EntityState.Added;
+            Db.SaveChanges();
+            Db.Dispose();
+            return obj;
+        }
+        public IEnumerable<T> Listar()
+        {
+            return Db.Set<T>().ToList();
+        }
+
+    }
+}
