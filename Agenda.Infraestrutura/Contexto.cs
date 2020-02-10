45897d25c3e454739f4bd613773c289f50e31e1c
Felipe Michel de Azevedo <felipe.azevedoit@outlook.com>
2020-02-10 18:57:19 -0300

    Aplicando Contexto e coonnection String

---
+++ Agenda.Infraestrutura/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Infraestrutura
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Dominio.Agenda.Agenda> Agenda { get; set; }
+        public DbSet<Dominio.Agenda.AgendaHorario> AgendaHorarios { get; set; }
+        public DbSet<Dominio.Organizacao.Organizacao> Organizacao { get; set; }
+        public DbSet<Dominio.Organizacao.OrganizacaoUsuario> OrganizacaoUsuario { get; set; }
+        public DbSet<Dominio.Usuario.Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
